2c0634782f293700bbc0b10dc6e744f867bce163
vm4321minus <staskulesh.brest@gmail.com>
2020-08-31 02:55:11 +0300

    izm

---
+++ 4lab_2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _4lab_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool otrz = false;
+            int i, j, m;
+            Console.WriteLine("Введите натуральное число");
+            m = int.Parse(Console.ReadLine());
+            for (i = 1; i < m; i++)
+                for (j = i; j < m; j++)
+                    if (i * i + j * j == m)
+                    {
+
+                        otrz = true;
+                        Console.WriteLine(i.ToString()  + " * " + i.ToString() + " + " + j.ToString()  + " * " + j.ToString() + " = " + m.ToString());
+                    }
+
+            if (!otrz) Console.WriteLine("нет числа");
+            Console.ReadKey();
+            
+        
+        }
+    }
+}
